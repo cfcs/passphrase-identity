c37e4218ba9a611a91ab79299a0687168d876fe0
Your Name <you@example.com>
2018-08-24 22:52:01 +0200

    --amend

--- gui/main.c
+++ gui/main.c
@@ -1,10 +1,18 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 
-static void destroy(GtkWidget *_widget, gpointer _data);
+static void on_destroy(GtkWidget *_widget, gpointer _data);
 static void
-destroy(GtkWidget *widget, gpointer data)
+on_destroy(GtkWidget *widget, gpointer data)
 {
 	gtk_main_quit();
+}
+
+void on_generate_clicked(void *_widget, gpointer _data);
+void
+on_generate_clicked(void *widget, gpointer data)
+{
+	printf("Generate! clicked\n");
 }
 
 int
@@ -18,11 +26,12 @@ main(int argc, char *argv[])
 	gtk_builder_add_from_file(gtkBuilder, "gui.glade", NULL);
 	GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder,
 		"main_window"));
+	gtk_builder_connect_signals(gtkBuilder, NULL);
 	g_object_unref(G_OBJECT(gtkBuilder));
 
 	/* Quitting main window kills us: */
 	g_signal_connect(G_OBJECT(window), "destroy",
-	    G_CALLBACK(destroy), NULL);
+	    G_CALLBACK(on_destroy), NULL);
 
 	/* Show main window and enter event loop: */
 	gtk_widget_show(window);
